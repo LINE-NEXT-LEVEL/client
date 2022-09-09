57952aa50ea769c0c0f0ded09f980c3f9b97f3d1
imnotmoon <imnotmoon@gmail.com>
2022-09-10 01:44:53 +0900

    Clear initial items

---
+++ src/pages/index.css.ts
@@ -0,0 +1,14 @@
+import { style } from "@vanilla-extract/css";
+
+export const heroBanner = style({
+    padding: '4rem 0',
+    textAlign: 'center',
+    position: 'relative',
+    overflow: 'hidden',
+});
+
+export const buttons = style({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+})
