4518f5cdedb6af903aecb6770c657868d11b0642
imnotmoon <imnotmoon@gmail.com>
2022-12-06 22:54:55 +0900

    Add CenterImage

---
+++ src/components/CenterImage/CenterImage.css.ts
@@ -0,0 +1,10 @@
+import { style } from '@vanilla-extract/css';
+
+export const CenterImageWrapper = style({
+  display: 'flex',
+});
+
+export const CenterImage = style({
+  margin: 'auto',
+  borderRadius: '10px',
+});
