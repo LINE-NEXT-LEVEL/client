906768b0b04110245dc319f752d0f0ff57a95893
yunyeongkwon <yunyeongkwon@linecorp.com>
2023-01-06 08:57:33 +0900

    Add challenges

---
+++ src/components/Challenge/DetailButton.css.ts
@@ -0,0 +1,10 @@
+import { style } from '@vanilla-extract/css';
+
+export const DetailButton = style({
+  display:"inline-block",
+  color: '#000000',
+  padding: '8px 16px',
+  backgroundColor: 'rgba(100, 100, 100, 0.2)',
+  borderRadius: '10px',
+  marginBottom: '10px',
+});
